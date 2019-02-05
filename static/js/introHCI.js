f39d500b526ab8d9ba53ded586b6a0b0fe46b349
Quentin Juquet <quentin.juquet@epitech.eu>
2019-02-04 19:47:11 -0800

    added Javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -2,8 +2,15 @@ $(document).ready(function() {
     initializePage();
 });
 
+$("a.thumbnail").click(projectClick);
+
 function initializePage() {
     $('#testjs').click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
             });
+}
+
+function projectClick(e) {
+    e.preventDefault();
+    $(this).css("background-color", "#7fff00");
 }
